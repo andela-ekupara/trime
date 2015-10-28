34e41be5026d0d7fc8321eaa69aad0940d8d2514
GertieNyesh <gertienyesh@gmail.com>
2015-10-28 14:21:25 +0300

    [Chore #106821994] Corrected gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
-var gulp = require('gulp');
-	less = require('gulp-less');
-	jade = require('gulp-jade');
-	nodemon = require('gulp-nodemon');
-	browserify = require('gulp-browserify');
-	paths = require('path');
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    jade = require('gulp-jade'),
+    nodemon = require('gulp-nodemon'),
+    browserify = require('gulp-browserify'),
+    path = require('path');
 
-	paths = {
-		public: 'public/**',
-		images: 'app/images/**/*',
-		scripts: 'app/**/*.js',
-		styles: 'app/styles/*.+(less|css)'
+var paths = {
+    public: 'public/**',
+    images: 'app/images/**/*',
+    scripts: 'app/**/*.js',
+    styles: 'app/styles/*.+(less|css)'
 
-	}
+};
 
-	gulp.task('less', function() {
-		gulp.src(paths.styles)
-			.pipe(less({
-				paths: [path.join(__dirname, './app/styles')]
-			}))
-			.pipe(gulp.dest('./public/css'))
-	});
-
+gulp.task('less', function () {
+    'use strict';
+    gulp.src(paths.styles)
+        .pipe(less({
+            paths: [path.join(__dirname, './app/styles')]
+        }))
+        .pipe(gulp.dest('./public/css'));
+});
