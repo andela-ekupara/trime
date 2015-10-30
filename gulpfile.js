afcfd41635e92c9785f028965e81008b54eeeff5
Brian Koech <brnkirwa@gmail.com>
2015-10-30 10:52:41 +0300

    Adds missing gulp tasks to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,73 @@
 var gulp = require('gulp'),
-    less = require('gulp-less'),
-    jade = require('gulp-jade'),
-    nodemon = require('gulp-nodemon'),
-    browserify = require('gulp-browserify'),
-    path = require('path');
+  less = require('gulp-less'),
+  gutil = require('gulp-util'),
+  jshint = require('jshint'),
+  jade = require('gulp-jade'),
+  nodemon = require('gulp-nodemon'),
+  browserify = require('gulp-browserify'),
+  source = require('vinyl-source-stream'),
+  imagemin = require('gulp-imagemin'),
+  path = require('path');
 
 var paths = {
-    public: 'public/**',
-    images: 'app/images/**/*',
-    scripts: 'app/**/*.js',
-    styles: 'app/styles/*.+(less|css)'
+  public: 'public/**',
+  images: 'app/images/**/*',
+  scripts: 'app/**/*.js',
+  styles: 'app/styles/*.+(less|css)',
+  jade: ['!app/shared/**', 'app/**/*.jade']
+}
 
-};
+gulp.task('less', function() {
+  gulp.src(paths.styles)
+    .pipe(less({
+      paths: [path.join(__dirname, './app/styles')]
+    }))
+    .pipe(gulp.dest('./public/css'))
+});
 
-gulp.task('less', function () {
-    'use strict';
-    gulp.src(paths.styles)
-        .pipe(less({
-            paths: [path.join(__dirname, './app/styles')]
-        }))
-        .pipe(gulp.dest('./public/css'));
+gulp.task('jade', function() {
+  gulp.src(paths.jade)
+    .pipe(jade())
+    .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['./app/**/*.js', './index.js', +
+      './server/**/*.js', './tests/**/*.js'
+    ])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('nodemon', function() {
+  nodemon({
+      script: 'app.js',
+      ext: 'js',
+      ignore: ['public/', 'node_modules/']
+    })
+    .on('change', ['lint'])
+    .on('restart', function() {
+      console.log('>> node restart');
+    });
+});
+
+gulp.task('images', function() {
+  gulp.src(paths.images)
+    .pipe(imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest('./public/images/'));
+});
+
+gulp.task('browserify', function() {
+  return browserify('./app/scripts/application.js').bundle()
+    .on('success', gutil.log.bind(gutil, 'Browserify Rebundled'))
+    .on('error', gutil.log.bind(gutil, 'Browserify ' +
+      'Error: in browserify gulp task'))
+    // vinyl-source-stream makes the bundle compatible with gulp
+    .pipe(source('application.js')) // Desired filename
+    // Output the file
+    .pipe(gulp.dest('./public/js/'));
 });
