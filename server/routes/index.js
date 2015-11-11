54ba92e5582f9c3a3d6cd8967c59a6811dbb3524
Brian Koech <brnkirwa@gmail.com>
2015-11-11 14:48:31 +0300

    add closure- improve code quality-(self invoking)

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,19 @@
-module.exports = function(app) {
+(function() {
   'use strict';
-  
-  require('./users')(app);
-  /* GET home page. */
-  app.get('*', function(req, res) {
-    res.sendFile('index.html', {
-      root: './public/'
+  module.exports = function(app) {
+    require('./users')(app);
+    /* GET home page. */
+    app.get('*', function(req, res) {
+      res.sendFile('index.html', {
+        root: './public/'
+      });
     });
-  });
-  
-  // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
-};
+
+    // catch error 404 and forward to error handler
+    app.use(function(req, res, next) {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+    });
+  };
+})();
