5ea141a1b9ff53c5407f994a7f783a24970b5f65
andela-gnyenyeshi <gertrudenyenyeshi@Gertrudes-MacBook-Pro.local>
2015-11-23 21:41:37 +0300

    Deletes peblic folder. Clears unnecessary comments

--- app/scripts/app.jsx
+++ app/scripts/app.jsx
@@ -1,12 +1,8 @@
 (function() {
   var React = require('react');
   var ReactDOM = require('react-dom');
-  var ReactRouter = require('flux-react-router');
-
   var Header = require('./components/header.jsx'); // Our custom react component
-  var Login = require('./components/modal-login.jsx'); // Login page
-
-
+ 
   window.React = React;
 
   ReactDOM.render( <Header/> ,
