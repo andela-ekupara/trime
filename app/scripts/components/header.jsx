2f571bcd3ecb7f8a94509d69e1e64dbec9d385c2
andela-gnyenyeshi <gertrudenyenyeshi@Gertrudes-MacBook-Pro.local>
2015-12-02 10:58:15 +0300

    Finishes [#109335950]

--- app/scripts/components/header.jsx
+++ app/scripts/components/header.jsx
@@ -1,23 +1,21 @@
 var React = require('react'),
-AppBar = require('material-ui').AppBar,
-RouteHandler =require('react-router').RouteHandler,
-Header = React.createClass({
-  render: function() {
-    return (
-				<div id="header">
-				  <div id="nav">
-				    <div className="mdl-grid">
-				      <div className="mdl-cell mdl-cell--8-col">
-				        <ul>
-				          <li className="login">TRIME  </li>
-				          <li className="right btn-start"><a href="#">Start Triming</a></li>
-				        </ul>
-				      </div>
-				    </div>
-				  </div>
-				</div>
-        );
-  }
-});
+		RouteHandler =require('react-router').RouteHandler,
+		Header = React.createClass({
+		render: function() {
+		  return (
+					<div id="header">
+					  <div id="nav">
+					    <div className="row">
+					      <div className="col s8">
+					        <ul>
+					          <li className="login">TRIME </li>
+					        </ul>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+		      );
+		}
+		});
 
 module.exports = Header;
--- app/scripts/routes/routes.js
+++ app/scripts/routes/routes.js
@@ -7,10 +7,12 @@ var RouteHandler = Router.RouteHandler;
 
 
 module.exports = (
+
 	<Route path="/" handler={require('../components/main.jsx')}>
 		<DefaultRoute handler={require('../components/landing.jsx')} />
-		<Route name="login" path="/start_triming" handler={require('../components/login.jsx')} />
+		<Route name="login" path="/login" handler={require('../components/login.jsx')} />
 		<Route name="test" path="/test" handler={require('../components/test.jsx')} />
 	</Route>
+		
 	);
 
