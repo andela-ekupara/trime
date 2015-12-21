a4a029d787f85e5febbd5277c65cd086b8b10f0f
Brian Koech <brnkirwa@gmail.com>
2015-12-21 16:34:39 +0300

    [#110244440] Replace tabs with spaces

--- server/models/project-trimes.js
+++ server/models/project-trimes.js
@@ -1,30 +1,30 @@
 (function() {
-	'use strict';
-	module.exports = function(sequelize, DataTypes) {
-		return sequelize.define('project-trimes', {
-			id: {
-				type: DataTypes.INTEGER,
-				primaryKey: true,
-				autoIncrement: true
-			},
+  'use strict';
+  module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('project-trimes', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
 
       description: {
         type: DataTypes.STRING,
         allowNull: true
       },
-      
-			complete: {
+
+      complete: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: false
       }
-		}, {
-			// auto created column fields should use snake case
-			underscored: true,
-			// disable attempts to pluralize tablename
-			freezeTableName: true,
-			// add created_at and modified_at columns
-			timestamps: true
-		});
-	};
+    }, {
+      // auto created column fields should use snake case
+      underscored: true,
+      // disable attempts to pluralize tablename
+      freezeTableName: true,
+      // add created_at and modified_at columns
+      timestamps: true
+    });
+  };
 })();
