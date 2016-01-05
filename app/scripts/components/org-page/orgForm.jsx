ac8ec50f2f9ab8853f65a143afa9f33a01a85026
Kevin Ndung'u <kevgathuku@gmail.com>
2016-01-05 16:20:35 +0300

    [#110554732] Add react-select with dummy data

--- app/scripts/components/org-page/orgForm.jsx
+++ app/scripts/components/org-page/orgForm.jsx
@@ -1,17 +1,43 @@
-(function(){
+(function() {
   'use strict';
   var React = require('react');
+  var Select = require('react-select');
   var OrgActions = require('../../actions/OrgActions');
   var OrgStore = require('../../stores/OrgStore');
 
+  const FLAVOURS = [
+    {
+      label: 'Chocolate',
+      value: 'chocolate'
+    }, {
+      label: 'Vanilla',
+      value: 'vanilla'
+    }, {
+      label: 'Strawberry',
+      value: 'strawberry'
+    }, {
+      label: 'Caramel',
+      value: 'caramel'
+    }, {
+      label: 'Cookies and Cream',
+      value: 'cookiescream'
+    }, {
+      label: 'Peppermint',
+      value: 'peppermint'
+    }
+  ];
   var OrgForm = React.createClass({
     getInitialState: function() {
       return {
         name: '',
         description: '',
-        result: ''
+        result: '',
+        searchable: true,
+        options: FLAVOURS,
+        value: []
       };
     },
+
     componentDidMount: function() {
       OrgStore.addChangeListener(this.handleUpdate);
     },
@@ -20,7 +46,7 @@
       var data = OrgStore.getData();
       if (data.error) {
         this.setState({result: 'Error Creating the Org!'});
-      } else{
+      } else {
         this.setState({result: 'Successfully created the Organisation'});
       }
     },
@@ -33,38 +59,54 @@
       this.setState({name: e.target.value});
     },
 
-    onSubmit: function(e) {
+    handleSubmit: function(e) {
       e.preventDefault();
       OrgActions.createOrg(this.state.name, this.state.description);
     },
 
+    handleSelectChange: function(value) {
+      console.log('You\'ve selected:', value);
+      this.setState({ value });
+    },
+
     render: function() {
       return (
-          <div className="row">
-            <form className="col s12" onSubmit={this.onSubmit}>
-              <div className="row">
-                <div className="input-field col s6">
-                  <input id="name" type="text" name="name" onChange={this.handleNameChange}
-                  className="validate" required />
-                  <label className="active" for="name"> Organisation Name </label>
-                </div>
+        <div className="row">
+          <form className="col s12" onSubmit={this.handleSubmit}>
+            <div className="row">
+              <div className="input-field col s6">
+                <input id="name" type="text" name="name" onChange={this.handleNameChange} className="validate" required/>
+                <label className="active" for="name">
+                  Organisation Name
+                </label>
               </div>
-              <div className="row">
-                <div className="input-field col s12">
-                  <textarea id="textarea1" name="description" onChange={this.handleDescriptionChange}
-                  className="materialize-textarea" required></textarea>
-                  <label for="Description">Description</label>
-                </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <textarea id="textarea1" name="description" onChange={this.handleDescriptionChange} className="materialize-textarea" required></textarea>
+                <label htmlFor="Description">Description</label>
               </div>
-              <button className="btn waves-effect waves-light" type="submit" name="action">Submit
-                <i className="material-icons right">send</i>
-              </button>
-              <p>{this.state.result}</p>
-            </form>
-          </div>
-      );
-    }
-
+            </div>
+            <div className="section">
+                <Select className="input-field col s6"
+                    multi
+                    onChange={this.handleSelectChange}
+                    options={this.state.options}
+                    placeholder="Select user(s)"
+                    searchable={this.state.searchable}
+                    simpleValue
+                    value={this.state.value}
+                />
+            </div>
+            <button className="btn waves-effect waves-light" type="submit" name="action">
+              Submit
+              <i className="material-icons right">send </i>
+            </button >
+            <p>{this.state.result}</p>
+          </form>
+        </div>
+        );
+      }
   });
 module.exports = OrgForm;
 })();
