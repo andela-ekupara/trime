824bbf6f2145b9e2ff2c5b14bf11f94122266e4c
andela-gnyenyeshi <gertrudenyenyeshi@Gertrudes-MacBook-Pro.local>
2016-01-06 16:49:15 +0300

    [#110649622] Triming buttons

--- app/scripts/components/start-trime/trime-page.jsx
+++ app/scripts/components/start-trime/trime-page.jsx
@@ -2,6 +2,15 @@
   'use strict';
   var React = require('react'),
     Select = require('react-select');
+    var Button = React.createClass({
+      render: function() {
+        return (
+          <div>
+            <button className="waves-effect waves-light btn">{this.props.label}<i className={this.props.icon}></i></button>
+          </div>
+        );
+      }
+    });
 
     var TrimePage = React.createClass({
       getInitialState: function() {
@@ -20,12 +29,19 @@
         var options = [
         {value: 'one', label: 'One'},
         {value: 'two', label:'Two'},
+        {value: 'three', label: 'Three'},
+        {value: 'four', label:'Two'},
+        {value: 'five', label: 'One'},
+        {value: 'six', label:'Two'},
+        {value: 'seven', label: 'One'},
+        {value: 'eight', label:'Two'},
         ];
 
         return (
-          <div >
-            <label>select Project</label>
-              <Select className="input-field col s12" autofocus options ={options} simpleValue disabled={this.state.disabled} searchable={this.state.searchable} clearable={this.state.clearable} onChange={this.logChange}/>
+          <div className="gerty">
+            <Select className="trimeProject" autofocus options ={options} simpleValue disabled={this.state.disabled} searchable={this.state.searchable} clearable={this.state.clearable} onChange={this.logChange} />
+            <Button label="Start" icon="fa fa-play"/>
+            <Button label="Stop" icon="fa fa-stop"/>
           </div>
         );
       }
