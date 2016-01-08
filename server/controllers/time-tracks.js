e6c4f7925a6fd1e6f33bf952f4aa4f09acfc13dd
Brian Koech <brnkirwa@gmail.com>
2016-01-08 14:12:14 +0300

    [#110244440] Add getProjects based on user

--- server/controllers/time-tracks.js
+++ server/controllers/time-tracks.js
@@ -3,8 +3,55 @@
 
   var timeTracks = require('../models').TimeTracks;
   var projectTrimes = require('../models').ProjectTrimes;
+  var projectUsers = require('../models').ProjectUsers;
+  var projects = require('../models').Projects;
 
   module.exports = {
+    getProjects: function(req, res) {
+      // get project from project_users
+      projectUsers.sync().then(function() {
+          return projectUsers.findAll({
+              where: {
+                // user_id: req.session.user_id,
+                user_id: req.params.userId
+              }
+            })
+            .then(function(project_users) {
+              if (!project_users) {
+                res.status(404).send({
+                  error: 'Projects not found'
+                });
+              } else {
+                var pro = [];
+                project_users.forEach(function(element) {
+                  projects.findOne({
+                      where: {
+                        id: element.project_id
+                      }
+                    })
+                    .then(function(project) {
+                      pro.push(project.dataValues);
+                    })
+                    .catch(function(err) {
+                      res.status(500).send({
+                        error: err.message
+                      });
+                    });
+                });
+                return pro;
+              }
+            })
+            .then(function(pro) {
+              return res.status(200).send(pro);
+            });
+        })
+        .catch(function(err) {
+          res.status(500).send({
+            error: err.message
+          });
+        });
+    },
+
     start: function(req, res) {
       projectTrimes.sync().then(function() {
         return projectTrimes.create({
@@ -17,28 +64,30 @@
                 error: 'Could not start tracking'
               });
             } else {
-              timeTracks.sync({force: true}).then(function() {
+              timeTracks.sync({
+                force: true
+              }).then(function() {
                 return timeTracks.create({
-                  startedAt: Date.now(),
-                  project_trime_id: projectTrime.dataValues.id
-                }).then(function(timeTrack) {
-                  if (!timeTrack) {
+                    startedAt: Date.now(),
+                    project_trime_id: projectTrime.dataValues.id
+                  }).then(function(timeTrack) {
+                    if (!timeTrack) {
+                      return res.status(500).send({
+                        error: 'Could not start Triming'
+                      });
+                    } else {
+                      res.session.projectTrimeId = projectTrime.dataValues.id;
+                      res.session.timeTrackId = timeTrack.id;
+                      return res.status(200).send({
+                        message: 'Timer started'
+                      });
+                    }
+                  })
+                  .catch(function(err) {
                     return res.status(500).send({
-                      error: 'Could not start Triming'
+                      error: err.errormsg
                     });
-                  } else {
-                    res.session.projectTrimeId = projectTrime.dataValues.id;
-                    res.session.timeTrackId = timeTrack.id;
-                    return res.status(200).send({
-                      message: 'Timer started'
-                    });
-                  }
-                })
-                .catch(function(err) {
-                  return res.status(500).send({
-                    error: err.errormsg
                   });
-                });
               });
             }
           });
@@ -105,7 +154,7 @@
           }
         })
         .then(function(ok) {
-          if(ok) {
+          if (ok) {
             projectTrimes.update({
                 complete: true
               }, {
@@ -114,7 +163,7 @@
                 }
               })
               .then(function(ok) {
-                if(ok) {
+                if (ok) {
                   res.session.projectTrimeId = null;
                   res.session.timeTrackId = null;
                   return res.status(200).send({
