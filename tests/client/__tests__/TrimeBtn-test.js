5544a931afe8800b7fe453d5234028ab5ecdf67d
andela-gnyenyeshi <gertrudenyenyeshi@Gertrudes-MacBook-Pro.local>
2016-02-01 11:42:06 +0300

    [#112778437] Updates Node on Circle

--- tests/client/__tests__/TrimeBtn-test.js
+++ tests/client/__tests__/TrimeBtn-test.js
@@ -1,24 +1,24 @@
-jest.dontMock('../../../app/scripts/components/LandingPage/TrimeBtn.jsx');
+(function() {
+  'use strict';
+  jest.dontMock('../../../app/scripts/components/LandingPage/TrimeBtn.jsx');
 
-var React = require('react'),
+  var React = require('react'),
     ReactDOM = require('react-dom'),
     TestUtils = require('react-addons-test-utils');
 
-var TrimeBtn = require('../../../app/scripts/components/LandingPage/TrimeBtn.jsx');
+  var TrimeBtn = require('../../../app/scripts/components/LandingPage/TrimeBtn.jsx');
 
-describe('TrimeBtn', function() {
+  describe('TrimeBtn', function() {
 
-  it('renders the TrimeBtn component', function() {
-    // Render the trime button in the document
-    var trimebtn = TestUtils.renderIntoDocument(
-      <TrimeBtn />
-    );
+    it('renders the TrimeBtn component', function() {
+      // Render the trime button in the document
+      var trimebtn = TestUtils.renderIntoDocument( < TrimeBtn / > );
 
-    var trimebtnNode = ReactDOM.findDOMNode(trimebtn);
+      var trimebtnNode = ReactDOM.findDOMNode(trimebtn);
 
-    // Verify that it has the correct content
-    expect(trimebtnNode.nodeType).toEqual(1);
-    expect(trimebtnNode.textContent).toEqual('Start Triming');
+      // Verify that it has the correct content
+      expect(trimebtnNode.nodeType).toEqual(1);
+      expect(trimebtnNode.textContent).toEqual('Start Triming');
+    });
   });
-
-});
+})();
