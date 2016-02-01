24433f353526a52e2bff68fc31bc87af709b7eeb
Kevin Ndung'u <kevgathuku@users.noreply.github.com>
2016-02-01 14:54:54 +0300

    [#112779329] Setup Frontend Test Coverage
    Add Jest coverage config and reorganize tests folder structure

--- tests/client/__tests__/components/LandingPage/TrimeBtn-test.js
+++ tests/client/__tests__/components/LandingPage/TrimeBtn-test.js
@@ -1,23 +1,28 @@
 (function() {
+
   'use strict';
-  jest.dontMock('../../../app/scripts/components/LandingPage/TrimeBtn.jsx');
+
+  var trimeBtnPath = '../../../../../app/scripts/components/LandingPage/TrimeBtn.jsx';
+
+  jest.dontMock(trimeBtnPath);
 
   var React = require('react'),
     ReactDOM = require('react-dom'),
     TestUtils = require('react-addons-test-utils');
 
-  var TrimeBtn = require('../../../app/scripts/components/LandingPage/TrimeBtn.jsx');
+  var TrimeBtn = require(trimeBtnPath);
 
   describe('TrimeBtn', function() {
 
     it('renders the TrimeBtn component', function() {
       // Render the trime button in the document
-      var trimebtn = TestUtils.renderIntoDocument( < TrimeBtn / > );
+      var trimebtn = TestUtils.renderIntoDocument(<TrimeBtn />);
 
       var trimebtnNode = ReactDOM.findDOMNode(trimebtn);
 
-      // Verify that it has the correct content
+      // It should be an Element
       expect(trimebtnNode.nodeType).toEqual(1);
+      // Verify that it has the correct content
       expect(trimebtnNode.textContent).toEqual('Start Triming');
     });
   });
