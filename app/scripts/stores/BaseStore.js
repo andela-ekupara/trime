48f7be96ab4246fd28cf1eba9c2bb47722df6428
andela-ekupara <edwin.kupara@andela.com>
2016-03-01 11:35:27 +0300

    [#113801077] Fix styling in BaseStore.js

--- app/scripts/stores/BaseStore.js
+++ app/scripts/stores/BaseStore.js
@@ -5,39 +5,39 @@
   var assign = require('object-assign');
 
   var BaseStore = assign({}, EventEmitter.prototype, {
-    data: null,
-    setData: function(data) {
-      this.data = data;
-      this.emitChange();
-    },
+      data: null,
+      setData: function(data) {
+        this.data = data;
+        this.emitChange();
+      },
 
-    getData: function() {
-      return this.data;
-    },
+      getData: function() {
+        return this.data;
+      },
 
-    emitChange: function(event) {
-      if (event) {
-        this.emit(event);
-      } else {
+      emitChange: function(event) {
+        if (event) {
+          this.emit(event);
+        } else {
           this.emit('change');
         }
-    },
+      },
 
-    addChangeListener: function(callback, event) {
-      if (event) {
-        this.on(event, callback);
-      } else {
+      addChangeListener: function(callback, event) {
+        if (event) {
+          this.on(event, callback);
+        } else {
           this.on('change', callback);
         }
-    },
+      },
 
-    removeChangeListener: function(callback) {
-      if (event) {
-        this.removeListener(event, callback);
-      } else {
-        this.removeListener('change', callback);
+      removeChangeListener: function(callback) {
+        if (event) {
+          this.removeListener(event, callback);
+        } else {
+          this.removeListener('change', callback);
+        }
       }
-    }
   });
 
   module.exports = BaseStore;
